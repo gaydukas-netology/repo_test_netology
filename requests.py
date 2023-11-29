2208940a4101c94ea99b28bf8cf65943b6d26480
gaydukas <gaydukplus@gmail.com>
2023-11-29 09:07:10 +0700

    fff

---
+++ requests.py
@@ -0,0 +1,11 @@
+POST /contacts HTTP/1.1
+Host: gorodinteresov.ru
+Content-Type: application/json
+Cookie: _csrf=GB_eStfdOYQ4BXhoaQYCdu3e; anonymousId=0a9aba4b-c19e-447f-b4eb-9e4b4ec5b6bd
+Content-Length: 74
+
+{
+    "name": "test",
+    "email": "test@test.ru",
+    "message": "test"
+}
